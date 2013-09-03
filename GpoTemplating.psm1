a7bd838044dc6abc6c1c114571936c1d0a684f23
Can Hanhan <p_github@finarfin.net>
2013-09-03 23:17:12 +0200

    Ported C# migration code to Powershell

--- GpoTemplating.psm1
+++ GpoTemplating.psm1
@@ -1,21 +1,283 @@
+function Migrate-Text {
+	[CmdletBinding()]
+	param (		
+        [AllowEmptyString()]
+        [Parameter(Mandatory=$true)]
+        [string]$Content,		
+
+        [ValidateNotNull()]
+        [Parameter(Mandatory=$true)]
+		[Hashtable]$Variables
+	)
+	
+    Begin { Write-Verbose "$($MyInvocation.MyCommand.Name):: Function started" } 
+    Process {
+        if ($Content -eq $null) { return $null }
+	    $temp = "";
+	    $output = $content;
+	    while ($temp -ne $output) { 
+		    $temp = $output
+		    $output = [regex]::Replace($output, '\{\{(?<key>\w+)\}\}', { 	
+			    param($match); 
+			
+			    $key = $match.Groups['key'].Value
+			    if (!$variables.ContainsKey($key)) {
+				    Write-Error "Cannot resolve template variable $key"
+			    }
+			
+			    $variables[$key] 
+		    })		
+	    }
+	
+	    $output 
+    }
+    End { Write-Verbose "$($MyInvocation.MyCommand.Name):: Function ended"} 
+}
+
+function Migrate-Registry {
+    [CmdletBinding(SupportsShouldProcess=$true)]
+	param (
+        [ValidateNotNullOrEmpty()]
+        [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+        [string]$Path,
+
+
+        [ValidateNotNull()]
+        [Parameter(Mandatory=$true)]
+		[Hashtable]$Variables
+	)
+
+    Begin { Write-Verbose "$($MyInvocation.MyCommand.Name):: Function started" } 
+	
+    Process {
+        if (!(Test-Path $path)) { return; }
+
+        if ($pscmdlet.ShouldProcess($path)) {	
+	        $parser = New-Object GroupPolicy.Parser.RegistryFile
+	        $parser.Open($path)
+	        $parser.Settings | ForEach-Object { 
+		        $_.Value = Migrate-Text -Variables $variables -Content $_.Value
+		
+		        if ($_.Type -eq "REG_SZ" -or $_.Type -eq "REG_EXPAND_SZ") {
+			        $_.Data = Migrate-Text -Variables $variables -Content $_.Data
+		        } elseif ($_.Type -eq "REG_MULTI_SZ") {
+			        $_.Data = ($_.Data | ForEach-Object { Migrate-Text -Variables $variables -Content $_ } )
+		        }				
+	        }
+
+	        $parser.Save()
+        }
+    }
+
+    End { Write-Verbose "$($MyInvocation.MyCommand.Name):: Function ended"} 
+}
+
+function Migrate-File {
+    [CmdletBinding(SupportsShouldProcess=$true)]
+	param (
+        [ValidateNotNullOrEmpty()]
+        [Parameter(Mandatory=$true)]  
+		[Microsoft.PowerShell.Commands.FileSystemCmdletProviderEncoding]$Encoding,
+
+        [ValidateNotNullOrEmpty()]
+        [Parameter(Mandatory=$true)]
+        [string]        
+		$Folder,
+
+        [ValidateNotNullOrEmpty()]
+        [Parameter(Mandatory=$true)]
+        [string]
+		$File,
+
+        [ValidateNotNull()]
+        [Parameter(Mandatory=$true)]
+		[Hashtable]$Variables        
+	)
+
+    Begin { Write-Verbose "$($MyInvocation.MyCommand.Name):: Function started" } 
+	
+    Process {
+	    $path = Join-Path $folder $file
+        if (!(Test-Path $path)) { return; }
+
+        if ($pscmdlet.ShouldProcess($path)) {	
+	        $fileInfo = New-Object System.IO.FileInfo($path)
+	        $wasReadOnly = $false;
+	
+	        if ($fileInfo.IsReadOnly)
+	        {
+		        $wasReadOnly = $true;
+		        $fileInfo.IsReadOnly = $false;
+	        }
+	
+	        $content = Migrate-Text -Variables $variables -Content (Get-Content -Raw -Path $path -Encoding $encoding)
+	        if ($_GPO_CUSTOM_ACTIONS.ContainsKey($file)) { 
+               @($_GPO_CUSTOM_ACTIONS[$file]) | ForEach-Object { $content = ($content | &$_)  }
+            }
+	        $content | Set-Content -Path $path
+	
+	        if ($wasReadOnly) { $fileInfo.IsReadOnly = $true; }	
+        }
+    }
+    End { Write-Verbose "$($MyInvocation.MyCommand.Name):: Function ended" } 
+}
+
+function Get-GroupSID {
+    [CmdletBinding()]
+	param(
+        [ValidateNotNullOrEmpty()]     
+        [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+        [string]$Group
+    )
+
+    Begin { Write-Verbose "$($MyInvocation.MyCommand.Name):: Function started" } 
+
+    Process {
+	    if ($group.Contains("\")) {
+		    $domainName = $group.Substring(0, $group.IndexOf("\"))
+		    $groupName = $group.Substring($group.IndexOf("\") + 1)
+	    } elseif ($group.Contains("@")) {
+		    $groupName = $group.Substring(0, $group.IndexOf("@"))
+		    $domainName = $group.Substring($group.IndexOf("@") + 1)			
+	    } else {
+		    return
+	    }
+	
+	    $domain = Get-ADDomain $domainName -ErrorAction Stop
+	    $groups = @(Get-AdObject -SearchBase $domain.DistinguishedName -Property SamAccountName, ObjectSid  -Filter  { DisplayName -eq $groupName -or Name -eq $groupName -or cn -eq $groupName -or SamAccountName -eq $groupName } -Server (Get-ADDomainController -Discover -DomainName $domain.Name -ErrorAction Stop).IPv4Address -ErrorAction Stop)
+	
+	    if ($groups.Length -eq 0) {
+		    Write-Warning "Cannot resolve $groupName in $domainName"
+	    } elseif ($groups.Length -gt 1) {
+		    Write-Error "There are multiple results for $groupName in $domainName. The search is done by DisplayName, Name, CN, SamAccountName attributes."
+	    } else {
+		    $groups | Select-Object -First 1 -Property @{Name="Name";Expr={"$($domain.NetbiosName)\$($_.SamAccountName)"}}, @{Name="SID";Expr={$_.ObjectSid}}
+	    }
+    }
+    End { Write-Verbose "$($MyInvocation.MyCommand.Name):: Function ended" } 
+}
+
+function Set-GroupSID {
+    [CmdletBinding()]
+	param (
+        [ValidateNotNull()]     
+        [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+		[Xml.XmlNode] $Group,
+
+        [ValidateNotNullOrEmpty()]
+        [Parameter(Mandatory=$true)]
+		[string]$NameAttribute,
+
+        [ValidateNotNullOrEmpty()]
+        [Parameter(Mandatory=$true)]
+		[string]$SidAttribute,
+		
+        [ValidateNotNull()]
+        [Parameter(Mandatory=$true)]
+		[Hashtable]$Variables     
+	)
+
+    Begin { Write-Verbose "$($MyInvocation.MyCommand.Name):: Function started" } 
+	
+    Process {
+	    if ($group.Attributes[$nameAttribute] -ne $null) {
+		    $groupName = $group.Attributes[$nameAttribute].Value
+	    }
+	
+	    if ([string]::IsNullOrEmpty($groupName)) { return $null }
+	
+	    $groupName = Migrate-Text -Variables $variables -Content $groupName
+		
+	    try  {
+		    $info = Get-GroupSID $groupName
+		    if ($info -eq $null) { return $null }
+		
+		    $group.Attributes[$nameAttribute].Value = $info.Name
+		    $group.Attributes[$sidAttribute].Value = $info.SID
+	    } catch {
+		    Write-Warning "Cannot resolve group $groupName to SID. Error: $($Error[0])"
+            return $null
+	    }
+    }
+
+    End { Write-Verbose "$($MyInvocation.MyCommand.Name):: Function ended" } 
+}
+
+function Migrate-PreferenceFile {
+    [CmdletBinding(SupportsShouldProcess=$true)]
+	param (
+        [ValidateNotNullOrEmpty()]
+        [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+        [string]$Path,
+
+
+        [ValidateNotNull()]
+        [Parameter(Mandatory=$true)]
+		[Hashtable]$Variables
+	)
+	
+    if (!(Test-Path $path)) { return }
+
+    if ($pscmdlet.ShouldProcess($path)) {	 
+	    $document = [xml] (Get-Content -Path $path)
+	    $document.GetElementsByTagName("FilterGroup") | Set-GroupSID -NameAttribute "name" -SIDAttribute "sid" -Variables $variables
+	    $document.GetElementsByTagName("Group") | Set-GroupSID -NameAttribute "groupName" -SIDAttribute "groupSid" -Variables $variables
+	    $document.GetElementsByTagName("Member") | Set-GroupSID -NameAttribute "name" -SIDAttribute "sid" -Variables $variables
+	
+	    $document.InnerXml = Migrate-Text -Variables $variables -Content $document.InnerXml
+	
+	    $document.Save($path)
+    }
+}
+
+function Migrate-Preferences {
+    [CmdletBinding(SupportsShouldProcess=$true)]
+	param (
+        [ValidateNotNullOrEmpty()]
+        [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+        [string]$Path,
+
+
+        [ValidateNotNull()]
+        [Parameter(Mandatory=$true)]
+		[Hashtable]$Variables
+	)
+	
+	if (!(Test-Path $path)) { return }
+	    
+	Get-ChildItem -Path $path -Directory | ForEach-Object { Migrate-Preferences -Path $_.FullName -Variables $variables }
+	Get-ChildItem -Path $path -File | Where-Object { $_.Extension -eq ".xml" } | ForEach-Object { Migrate-PreferenceFile -Path $_.FullName -Variables $variables }
+}
+
 function Migrate-GPO {
 	[CmdletBinding(SupportsShouldProcess=$true)]
 	param (
 		[parameter(Mandatory=$true)]
 		[ValidateNotNullOrEmpty()]
 		[string]
-		$gpoPath,
+		$Path,
 
 		[parameter(Mandatory=$true)]
 		[ValidateNotNullOrEmpty()]
 		[Hashtable]	
-		$variables
+		$Variables
 	)
 	
-	if ($pscmdlet.ShouldProcess($gpoPath)) {
-		$migrator = New-Object -TypeName "GroupPolicy.Migration.Migrator" -ErrorAction Stop
-		$migrator.Migrate($gpoPath, $variables);	
-	}
+    $gpoPath = Join-Path $Path $_GPO_PATH
+    if (!(Test-Path $gpoPath)) { throw "Path $gpoPath does not exist" }
+
+	Migrate-Preferences -Path (Join-Path (Join-Path $gpoPath $_GPO_PATH_MACHINE) $_GPO_PATH_PREFERENCES) -Variables $variables
+	Migrate-Preferences -Path (Join-Path (Join-Path $gpoPath $_GPO_PATH_USER) $_GPO_PATH_PREFERENCES) -Variables $variables
+	Migrate-Registry -Path (Join-Path (Join-Path $gpoPath $_GPO_PATH_MACHINE) $_GPO_REGISTRY_FILE) -Variables $variables
+	Migrate-Registry -Path (Join-Path (Join-Path $gpoPath $_GPO_PATH_USER) $_GPO_REGISTRY_FILE) -Variables $variables
+	
+	$_GPO_FILES.Keys | ForEach-Object {
+		$encoding = $_
+		$_GPO_FILES[$encoding] | ForEach-Object {
+			Migrate-File -Encoding $encoding -Folder (Join-Path $gpoPath $_GPO_PATH_MACHINE) -File $_ -Variables $variables
+			Migrate-File -Encoding $encoding -Folder (Join-Path $gpoPath $_GPO_PATH_USER) -File $_ -Variables $variables
+		}
+	}	
 }
 
 function Copy-GPOEx {
@@ -24,52 +286,52 @@ function Copy-GPOEx {
 		[parameter(Mandatory=$true, HelpMessage="Source policy name")]
 		[ValidateNotNullOrEmpty()]
 		[string]
-		$sourcePolicy,		
+		$SourcePolicy,		
 	
 		[parameter(Mandatory=$false, HelpMessage="Source domain name or FQDN. (Default: Current domain)")]
 		[ValidateNotNullOrEmpty()]
 		[string]
-		$sourceDomain=$env:USERDNSDOMAIN,
+		$SourceDomain=$env:USERDNSDOMAIN,
 		
 		[parameter(Mandatory=$false, HelpMessage="A domain controller in source domain. (Default: Any domain controller in domain)")]
 		[ValidateNotNullOrEmpty()]
 		[string]		
-		$sourceServer=$sourceDomain,
+		$SourceServer=$sourceDomain,
 
 		[parameter(Mandatory=$false, HelpMessage="Credentials to connect the source domain (Default: none)")]
 		[ValidateNotNull()]
 		[PSCredential]	
-		$sourceCredential,
+		$SourceCredential,
 	
 		[parameter(Mandatory=$false, HelpMessage="Target policy name (Default: Source policy name)")]
 		[ValidateNotNullOrEmpty()]
 		[string]			
-		$targetPolicy=$sourcePolicy,		
+		$TargetPolicy=$sourcePolicy,		
 		
 		[parameter(Mandatory=$false, HelpMessage="Target domain name or FQDN. (Default: Source domain)")]
 		[ValidateNotNullOrEmpty()]
 		[string]			
-		$targetDomain=$sourceDomain,
+		$TargetDomain=$sourceDomain,
 		
 		[parameter(Mandatory=$false, HelpMessage="A domain controller in target domain. (Default: Any domain controller in domain)")]
 		[ValidateNotNullOrEmpty()]
 		[string]			
-		$targetServer=$targetDomain,
+		$TargetServer=$targetDomain,
 		
 		[parameter(Mandatory=$false, HelpMessage="Credentials to connect the target domain (Default: none)")]
 		[ValidateNotNull()]
 		[PSCredential]			
-		$targetCredential,
+		$TargetCredential,
 		
 		[parameter(Mandatory=$false, HelpMessage="Path to store GPO backups (Default: C:\Temp)")]
 		[ValidateNotNullOrEmpty()]
 		[string]			
-		$backupPath="C:\Temp",
+		$BackupPath="C:\Temp",
 		
 		[parameter(Mandatory=$false, HelpMessage="Path to the configuration file (Default: none)")]
 		[ValidateNotNullOrEmpty()]
 		[string]			
-		$configFile
+		$ConfigFile
 	)
 	
 	$operation = { param($sourcePolicy, $backupPath, $sourceDomain, $sourceServer); Import-Module GroupPolicy; Backup-GPO -Name $sourcePolicy -Path $backupPath -Domain $sourceDomain -Server $sourceServer -ErrorAction Stop }	
@@ -83,7 +345,7 @@ function Copy-GPOEx {
 	$gpoPath = Join-Path $backup.BackupDirectory $backup.Id.ToString("B");		
 	if (![string]::IsNullOrEmpty($configFile)) {	
 		$variables = ((Get-Content $configFile -ErrorAction Stop)  -join "`n" -replace "\\", "\\" | ConvertFrom-StringData)
-		Migrate-GPO -GpoPath $gpoPath -Variables $variables -ErrorAction Stop
+		Migrate-GPO -Path $gpoPath -Variables $variables -ErrorAction Stop
 	}
 	
 	$operation = { param($backup, $targetPolicy, $targetDomain, $targetServer); Import-Module GroupPolicy;  Import-GPO -BackupId $backup.Id.ToString("B") -TargetName $targetPolicy -Domain $targetDomain -CreateIfNeeded -Path $backup.BackupDirectory -Server $targetServer -ErrorAction Stop }
@@ -94,3 +356,71 @@ function Copy-GPOEx {
 		&$operation $backup $targetPolicy $targetDomain $targetServer;
 	}		
 }
+
+$_GPO_FILES = @{
+	"UTF8"= @(	"Microsoft\Windows NT\Audit\Audit.csv");
+	"Unicode"= @("Microsoft\IEAK\branding\ratings\ratings.inf",
+				 "Microsoft\IEAK\branding\ratings\ratrsop.inf",
+				 "Microsoft\IEAK\branding\authcode\authcode.inf",
+				 "Microsoft\IEAK\branding\programs\programs.inf",
+				 "scripts\scripts.ini",
+				 "scripts\psscripts.ini",
+				 "Documents & Settings\fdeploy.ini",
+				 "Documents & Settings\fdeploy1.ini",
+                 "Microsoft\Windows NT\SecEdit\GptTmpl.inf");
+	"ascii"= @("Microsoft\IEAK\branding\zones\seczrsop.inf",
+			   "Microsoft\IEAK\branding\zones\seczones.inf",	
+			   "Microsoft\IEAK\install.ins")		 
+}
+
+$_GPO_CUSTOM_ACTIONS = @{
+	"Microsoft\Windows NT\SecEdit\GptTmpl.inf"={
+        param(
+            [ValidateNotNull()]
+            [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+            [string]$Content
+        ) 
+    
+        Begin { Write-Verbose "Translate-SecEditSIDs Function started" } 
+
+        Process 
+        {
+            $lines = $content -split "`r`n"
+            $start = $lines.IndexOf("[Group Membership]")
+            if ($start -eq -1) { return; }
+            $end = $lines[($start+1)..($lines.Length)].IndexOf((@($lines[($start+1)..($lines.Length)] -match "^\[.+") | Select-Object -First 1))
+            if ($end -eq -1) { $end = $lines.Length } else { $end += $start }
+
+            $newLines = $lines[$start..$end] | ForEach-Object {
+                if (!$_.StartsWith("*")) { return $_ }
+
+                $startPos = $_.IndexOf("=");  
+                $groups = $_.Substring($startPos + 1).Trim() -split "," | ForEach-Object {
+                    if ($_.StartsWith("*") -or [string]::IsNullOrWhiteSpace($_)) { return $_ }
+                    $groupSid = Get-GroupSID $_
+                    if ($groupSid -ne $null) {
+                        "*$($groupSid.SID)"
+                    } else {
+                        $_
+                    }
+                }
+
+                $_.Substring(0, $startPos + 1) + " " + ($groups -join ",")
+            }
+
+            for($i=0; $i -lt $newLines.Length; $i++) {
+                $lines[$start+$i] = $newLines[$i]
+            }
+
+            $lines -join "`n"
+        }
+
+        End { Write-Verbose "Translate-SecEditSIDs Function ended"} 
+    }
+}
+
+$_GPO_PATH = "DomainSysvol\GPO"
+$_GPO_PATH_PREFERENCES = "Preferences";
+$_GPO_PATH_USER = "User";
+$_GPO_PATH_MACHINE = "Machine";
+$_GPO_REGISTRY_FILE = "registry.pol";
